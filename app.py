2da509ec329c49d6f70ea147a51acb003e8ec363
tygy1968-svg <tygy1968@gmail.com>
2026-03-31 02:38:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
+TOKEN = "8028195967:AAFw9yI4YS38RrWtriNxQDvJPL_RNzw5ZB8"
+URL = f"https://api.telegram.org/bot{TOKEN}/"
+
+@app.route('/', methods=['POST'])
+def webhook():
+    data = request.get_json()
+
+    if "message" in data:
+        chat_id = data["message"]["chat"]["id"]
+        text = data["message"].get("text", "")
+
+        send_message(chat_id, f"Кузя тут 👋 Ты написал: {text}")
+
+    return "ok"
+
+def send_message(chat_id, text):
+    requests.post(URL + "sendMessage", json={
+        "chat_id": chat_id,
+        "text": text
+    })
+
+@app.route('/')
 def home():
     return "Кузя жив 👋"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
