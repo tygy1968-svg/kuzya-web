ba0b251a91ead5f90e8ee3815f3160f4e8692ba0
tygy1968-svg <tygy1968@gmail.com>
2026-03-31 02:10:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "I am alive"
+    return "Кузя жив 👋"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
